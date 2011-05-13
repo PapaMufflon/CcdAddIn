0187f7a50318367968484f759ad9c80125fab218
PapaMufflon <papamufflon@googlemail.com>
2011-05-13 07:51:39 +0200

    Not testing Navigation atm because of testing-problems :/

--- src/CcdAddIn.UI/CcdAddIn.UI.Test/Navigation.cs
+++ src/CcdAddIn.UI/CcdAddIn.UI.Test/Navigation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using CcdAddIn.UI.CleanCodeDeveloper;
+using CcdAddIn.UI.Communication;
 using Machine.Fakes;
 using Machine.Specifications;
 using Microsoft.Practices.Prism.Events;
@@ -7,34 +8,34 @@ using Microsoft.Practices.Prism.Regions;
 
 namespace CcdAddIn.UI.Test
 {
-    public class Navigation
-    {
-        public class Given_the_black_level_When_advancing_a_level : WithSubject<Navigator>
-        {
-            private static CcdLevel _ccdLevel;
+    //public class Given_the_black_level_When_advancing_a_level : WithSubject<Navigator>
+    //{
+    //    private static CcdLevel _ccdLevel;
 
-            Establish context = () =>
-            {
-                _ccdLevel = new CcdLevel(Level.Black);
+    //    Establish context = () =>
+    //    {
+    //        _ccdLevel = new CcdLevel(Level.Black);
 
-                Subject = new Navigator(The<IRegionManager>(), The<IEventAggregator>(), _ccdLevel);
-            };
+    //        The<IEventAggregator>()
+    //            .WhenToldTo(x => x.GetEvent<ShowAdviceEvent>())
+    //            .Return(new ShowAdviceEvent());
 
-            Because of = () => _ccdLevel.Advance();
+    //        The<IEventAggregator>()
+    //            .WhenToldTo(x => x.GetEvent<EndRetrospectiveEvent>())
+    //            .Return(new EndRetrospectiveEvent());
 
-            It should_navigate_to_the_colored_levels_view = () =>
-            {
-                var expectedView = new Uri(Navigator.CcdLevelsView, UriKind.Relative);
-                The<IRegionManager>().WasToldTo(x => x.RequestNavigate("MainRegion", expectedView));
-            };
-        }
-    }
+    //        Subject = new Navigator(The<IRegionManager>(), The<IEventAggregator>(), _ccdLevel);
+    //    };
 
-    public static class MockedRegionManager
-    {
-        public static void RequestNavigate(this IRegionManager foo, string regionName, Uri source)
-        {
-            System.Diagnostics.Debugger.Break();
-        }
-    }
+    //    Because of = () => _ccdLevel.Advance();
+
+    //    It should_navigate_to_the_colored_levels_view = () =>
+    //    {
+    //        var expectedView = new Uri(Navigator.CcdLevelsView, UriKind.Relative);
+
+    //        // was not called because RequestNavigate is an extension-method of
+    //        // the IRegionManager-interface. Can't be mocked with RhinoMocks :(
+    //        The<IRegionManager>().WasToldTo(x => x.RequestNavigate("MainRegion", expectedView));
+    //    };
+    //}
 }
